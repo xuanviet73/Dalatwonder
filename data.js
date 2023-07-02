03f7b524245eb3ebaf1ffe8f167513d535cd4e83
xuanviet73 <2012393@dlu.edu.vn>
2023-07-03 00:43:52 +0700

    DaLatWonderVR

---
+++ data.js
@@ -0,0 +1,211 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dalatwonder",
+      "name": "DaLatWonder",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6518559879562273,
+          "pitch": 0.45916516398268215,
+          "rotation": 0,
+          "target": "1-khu-bit-th-dalatwonder"
+        },
+        {
+          "yaw": -0.04116813411821596,
+          "pitch": 0.7944574575523191,
+          "rotation": 0,
+          "target": "2-ta-chnh"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-khu-bit-th-dalatwonder",
+      "name": "Khu biệt thự DaLatWonder",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4099369366641774,
+          "pitch": 0.0836627911463168,
+          "rotation": 0,
+          "target": "4-ni-tht"
+        },
+        {
+          "yaw": -1.7362932193206237,
+          "pitch": 0.06322132285283288,
+          "rotation": 0,
+          "target": "2-ta-chnh"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-ta-chnh",
+      "name": "Tòa chính",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.113432840888727,
+          "pitch": 0.01950610738902192,
+          "rotation": 1.5707963267948966,
+          "target": "3-lavender-louge"
+        },
+        {
+          "yaw": 0.08699707839947024,
+          "pitch": -0.17692282156597727,
+          "rotation": 0,
+          "target": "0-dalatwonder"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-lavender-louge",
+      "name": "Lavender Louge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2530206679940017,
+          "pitch": -0.00615301646164923,
+          "rotation": 4.71238898038469,
+          "target": "2-ta-chnh"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-ni-tht",
+      "name": "Nội thất",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1368,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.639557836391532,
+          "pitch": 0.3560242242075873,
+          "rotation": 0,
+          "target": "1-khu-bit-th-dalatwonder"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
